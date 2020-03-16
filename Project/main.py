Bug fixed. Gray scaling images. Softmax and CrossEntropyLoss implemented.

Project/main.py
@@ -1,8 +1,9 @@
-from data import extract_data, init_data, transform_resize
+from data import transform_resize
 from net import Net
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+from torchvision import datasets
 import numpy as np
+import torch
 
 folder = 'GTSRB'
 train_folder = folder + '/Final_Training/Images' 
@@ -11,33 +12,35 @@ valid_folder = folder + '/Final_Validation/Images'
 # Parameters
 learning_rate = 0.001
 batch_size = 64
-layers = np.asarray([[64, 25, 'relu', 0, 0], [25, 42, 'sigmo', 0, 0]])
-
-# Init the data
-extract_data()
-init_data()
+cls_num = 43
+layers = np.asarray([[4096, 50, 'relu', 0, 0], [50, cls_num, 'softmax', 0, 0]])
+epoch_num = 500
 
 # Create the DataLoaders
 train_loader = DataLoader(datasets.ImageFolder(train_folder, transform=transform_resize), batch_size=batch_size, shuffle=True)
-valid_loader = DataLoader(datasets.ImageFolder(valid_folder, transform=transform_resize), batch_size=batch_size)
+# valid_loader = DataLoader(datasets.ImageFolder(valid_folder, transform=transform_resize), batch_size=batch_size)
 
 model = Net(layers, learning_rate)
 
-# Train
-# Switching to training mode (dropout enabling)
-model.train()
-for idx, (data, target) in enumerate(train_loader):
-    output = model(data)
-    model.eval_loss(output, target)
-    print(model.cost)
-    # model.back_prop('adam')     # Crashing Here
-    # print(data.shape)
-    # print('{:>4} | Size: {:>3}x{:<3} | ClassID: {}'.format(idx, data.shape[3], data.shape[2] ,target.item()))
+cost_train = []
 
+for curr_ep in range(epoch_num):
 
+	# Train
+	# Switching to training mode (dropout enabling)
+	model.train()
+	cost_acc = 0
 
-# Validation
-# Switching to evaluation mode (dropout disabling)
-model.eval()
-for idx, (data, target) in enumerate(valid_loader):
-    print('{:>4} | Size: {:>3}x{:<3} | ClassID: {}'.format(idx, data.shape[3], data.shape[2] ,target.item()))
+	for __, (data, target) in enumerate(train_loader):
+
+		model.forward(data.view(batch_size, 4096), target)
+
+		model.back_prop('adam', weight_decay = 0)
+
+		cost_acc += model.cost
+
+	
+	cost_train.append(cost_acc / len(train_loader))
+
+   
+

Project/net.py
@@ -25,6 +25,7 @@ class Net(nn.Module):
 		self.layers = nn.ModuleList()
 		self.act_funcs = nn.ModuleList()
 		self.drop = nn.ModuleList()
+		self.loss_fn = nn.CrossEntropyLoss()
 
 		for i in range(layers.shape[0]):
 
@@ -39,34 +40,39 @@ class Net(nn.Module):
 				self.act_funcs.append(nn.Tanh())
 			elif layers[i][2] == 'relu':
 				self.act_funcs.append(nn.ReLU())
-			elif (layers[i][2] == 'leakyrelu'):
+			elif layers[i][2] == 'leakyrelu':
 				self.act_funcs.append(nn.LeakyReLU(layers[i][3]))
+			elif layers[i][2] == 'softmax':
+				self.act_funcs.append(nn.Softmax(dim=1))
 			else:
 				print("Activation function not defined. Aborting...")
 				sys.exit()
 
 
-	def forward(self, data):
+	def forward(self, data, check_data):
 		"""
 		Feed data to Network. Calculate the estimated output and Cost function
 		"""
 		self.output = data	# DO NOT ERRASE. Do not pay attentition to this
+
 		for i in range(len(self.layers)):
+			# if (i < len(self.layers) - 1) and
 
-			try:
-				self.output = self.drop[i](self.act_funcs[i](self.layers[i](self.output)))
-
-			except TypeError:
-				# This should happen when activation function is set to None
+			if (i == len(self.layers) - 1) and self.training:
+			 	# When the last layer is reacehd and if it is in traning mode, the activation function is not applied
 				self.output = self.drop[i](self.layers[i](self.output))
 
-			return self.output
+			else:
+				try:
+					self.output = self.drop[i](self.act_funcs[i](self.layers[i](self.output)))
 
-	def eval_loss(self, data, check_data):
-		if torch.is_tensor(check_data):
-			loss_fn = nn.MSELoss()
-			self.cost = loss_fn(data, check_data.view(len(check_data),1))
+				except TypeError:
+					# This should happen when activation function is set to None
+				self.output = self.drop[i](self.layers[i](self.output))
 
+		self.cost = slef.loss_fn(data, check_data.view(len(check_data),1))
+
+		
 
 	def back_prop(self, opt, momentum = 0, weight_decay=0):
 		"""
@@ -97,6 +103,7 @@ class Net(nn.Module):
 		"""
 		Metrics calculation and printing
 		"""
+		# TODO: update using scikit learn
 		ones_tensor = torch.ones(len(check_data), 1)
 		zeros_tensor = torch.zeros(len(check_data), 1)
 		check_data_TF = torch.where(check_data > 0, ones_tensor, zeros_tensor)[1,:].view(len(check_data),1)